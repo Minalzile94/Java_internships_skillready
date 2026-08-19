a89fe201e72bb2ccc629c30fa7a5bc22e355f27a
Minalzile94 <minalzile94@gmail.com>
2026-08-19 22:33:48 +0530

    Add files via upload

---
+++ Runtime_input.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class Runtime_input {
+    public static void main(String[] args) {
+        //how to take run time inputs values from user
+        //s1 is refference variable just like nick name
+        Scanner s1=new Scanner(System.in);
+        System.out.println("enter any value:");
+        int a = s1.nextInt();
+        System.out.println("your value is:"+a);
+    }
+}
