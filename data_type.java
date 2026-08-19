a9ccc7e94421323c66a3ed6f7c9ec0a51e31df0e
Minalzile94 <minalzile94@gmail.com>
2026-08-19 22:32:59 +0530

    Add files via upload

---
+++ data_type.java
@@ -0,0 +1,10 @@
+class data_type {
+    public static void main(String[] args) {
+        int age =15;
+        float salary =25000.500f;
+        double pi =3.14156789;
+        char grade ='A';
+        boolean pass =true;
+        String my_name="Minal";
+    }
+}
