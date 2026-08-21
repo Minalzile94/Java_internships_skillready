54205ef4e56b14d460831df3ceaf02b747f40a24
Minalzile94 <minalzile94@gmail.com>
2026-08-21 21:53:53 +0530

    Add files via upload

---
+++ LogicalOP2.java
@@ -0,0 +1,9 @@
+class LogicalOP2 {
+    public static void main(String[] args) {
+        int age=17;
+        boolean citizen=true;
+        System.out.println("logical or=");
+        System.out.println("age>=18// citizen");
+    }
+    
+}
