fe1a01f2e28dee3699c8dd9f38154831d4c3f823
Minalzile94 <minalzile94@gmail.com>
2026-08-22 22:24:48 +0530

    Add files via upload

---
+++ TernaryOP.java
@@ -0,0 +1,9 @@
+class TernaryOP {
+    public static void main(String[] args) {
+       int marks=30;
+       String result =(marks>=40)?"PASS": "FAIL";
+       System.out.println(" student is " + result); 
+    }
+    
+}
+  
