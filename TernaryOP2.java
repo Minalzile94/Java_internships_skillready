f812232af9615554a0e8b75df9d6e951da0c1953
Minalzile94 <minalzile94@gmail.com>
2026-08-22 22:25:17 +0530

    Add files via upload

---
+++ TernaryOP2.java
@@ -0,0 +1,9 @@
+class TernaryOP2 {
+    public static void main(String[] args) {
+        //voting eligibility
+        int age=17;
+        String result =(age>=18)? "user is eligible for voting":"not eligible";
+        System.out.println("result");
+    }
+    
+}
