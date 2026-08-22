f448118315f55e0691a9617dbcb50d4a0152273d
Minalzile94 <minalzile94@gmail.com>
2026-08-22 22:26:06 +0530

    Add files via upload

---
+++ TernaryOP4.java
@@ -0,0 +1,11 @@
+class TernaryOP4 {
+    public static void main(String[] args) {
+        //check employee bonus
+        int emp_expr=6;
+        String result =(emp_expr>=5)?"bonus eligible":"not eligible for bonus";
+        System.out.println("result");
+        
+        
+    }
+    
+}
