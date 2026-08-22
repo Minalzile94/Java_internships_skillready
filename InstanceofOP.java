22c5d109c33f2913c198fb561d281cc0343886d7
Minalzile94 <minalzile94@gmail.com>
2026-08-22 22:27:09 +0530

    Add files via upload

---
+++ InstanceofOP.java
@@ -0,0 +1,13 @@
+//instance operator checks whether an object
+//belongs to a specific class or not
+class InstanceofOP {
+    public static void main(String[] args) {
+        String course="JAVA";
+        String b = " ";
+        int a=10;
+        System.out.println("course instanceof String");
+        System.out.println("b instanceof String");
+        
+    }
+    
+}
