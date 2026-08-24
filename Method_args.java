eb71ea2d8e96419f2ef8c6a478fb9d885015ee8f
Minalzile94 <minalzile94@gmail.com>
2026-08-24 21:36:44 +0530

    Add files via upload

---
+++ Method_args.java
@@ -0,0 +1,13 @@
+class Method_args {
+    void addition(){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("Addition="+c);
+    }
+    public static void main(String[] args) {
+        Method_args m1=new Method_args();
+        m1.addition();
+        }
+    }
+    
