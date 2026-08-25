c20129a54f8ffa6e179d0ffba61fbefa38a70259
Minalzile94 <minalzile94@gmail.com>
2026-08-25 22:19:00 +0530

    Add files via upload

---
+++ if_block.java
@@ -0,0 +1,13 @@
+ class if_block {
+    public static void main(String[] args) {
+        int a=100;
+        //syntax of if block
+        //if (condition){statement}
+        if (a==100){
+            System.out.println("yes value of a is 100");
+        }
+        else{
+            System.out.println("value of a is not 100");
+        }
+    }
+}
