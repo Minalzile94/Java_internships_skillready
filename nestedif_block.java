db5444144ffc014511395b0267fdba8572b7b24f
Minalzile94 <minalzile94@gmail.com>
2026-08-25 22:19:53 +0530

    Add files via upload

---
+++ nestedif_block.java
@@ -0,0 +1,20 @@
+class nestedif_block {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        //nested if syntax
+        //if(){
+        //if(){
+        //if(){
+        //}}}
+        if(a==10){
+            if(b==20){
+                if(c==30){
+                    System.out.println("all conditions are true");
+                }else{System.out.println("c is not equal");}
+            }else{System.out.println("b is not equal to 20");}
+                }
+            }
+        }
+        
