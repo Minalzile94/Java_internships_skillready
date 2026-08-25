d8091c86e9eb7fe857e3de025d8916c04bd7f2e8
Minalzile94 <minalzile94@gmail.com>
2026-08-25 22:20:45 +0530

    Add files via upload

---
+++ voting2.java
@@ -0,0 +1,14 @@
+
+
+class voting2 {
+    public static void main(String[] args) {
+        String country="india";
+        int age=18;
+        if(country=="india"){
+            if(age>=18){
+                System.out.println("you are eligible for voting");
+            }
+        }
+    }
+    
+}
