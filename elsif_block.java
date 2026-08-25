5a844596573dbc5ab2188754fceb51fda6c02a8a
Minalzile94 <minalzile94@gmail.com>
2026-08-25 22:19:27 +0530

    Add files via upload

---
+++ elsif_block.java
@@ -0,0 +1,12 @@
+class elsif_block {
+    public static void main(String[] args) {
+        int a=100;
+        if (a==100){
+            System.out.println("a==100");
+        }
+        else if(a==100){System.out.println("a==200");}
+        else if(a==300){System.out.println("a==300");}
+        else {System.out.println("not possible condition are there");}
+        }
+    }
+    
