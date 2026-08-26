2dd2a83c3aa4dc0791ab489646a544ec39046be6
Minalzile94 <minalzile94@gmail.com>
2026-08-26 20:34:53 +0530

    Add files via upload

---
+++ forloopex.java
@@ -0,0 +1,12 @@
+//syntax of for loop
+// for(initialization;condition;increment/decrement){
+// code}
+ class forloopex {
+    public static void main(String[] args) {
+        int a=5;
+        for(int i=0;i<=5;i++){
+            System.out.println("value of a = +a");
+        }
+    }
+    
+}
