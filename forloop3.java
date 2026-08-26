65d250e35a48debf7f942890a30b3c3d9e0f0375
Minalzile94 <minalzile94@gmail.com>
2026-08-26 20:37:26 +0530

    Add files via upload

---
+++ forloop3.java
@@ -0,0 +1,9 @@
+//print the number from 10 to 1 reserve number 10 9 8 7 6 5 4 3 2 1
+ class forloop3 {
+    public static void main(String[] args) {
+        for(int i=10;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+    
+}
