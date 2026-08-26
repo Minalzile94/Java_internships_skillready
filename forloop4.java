114d48de0baf9f9f5a4ef16bb89a56542e075fca
Minalzile94 <minalzile94@gmail.com>
2026-08-26 20:38:00 +0530

    Add files via upload

---
+++ forloop4.java
@@ -0,0 +1,12 @@
+//print the number from 2 to 10 even number 
+class forloop4 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i%2==0){
+            System.out.println("even number="+i);
+        }else{
+            System.out.println("odd number="+i);
+        }
+    }
+}
+}
