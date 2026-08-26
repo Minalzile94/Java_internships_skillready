28481e11d6964ec4a9ff358e930ea1557105fa91
Minalzile94 <minalzile94@gmail.com>
2026-08-26 20:36:35 +0530

    Add files via upload

---
+++ infiniteloop.java
@@ -0,0 +1,10 @@
+//how to declare the infinite loop
+// without any variable or without condition
+ class infiniteloop {
+    public static void main(String[] args) {
+        for (;;) {
+            System.out.println("this runs forever");
+        }  
+        }
+    }
+    
