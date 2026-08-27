3a82c5169b6fc87102ccdd176520aad7c46254a0
Minalzile94 <minalzile94@gmail.com>
2026-08-27 22:08:18 +0530

    Add files via upload

---
+++ reversenumber.java
@@ -0,0 +1,12 @@
+ // print the reverse number from range 1 to 10
+ // 10 9 8 7 6 5 4 3 2 1
+ class reversenumber {
+    public static void main(String[] args) {
+        int i=10;
+        while(i>=1){
+            System.out.println(i+" ");
+            i--;
+        }
+    }
+    
+}
