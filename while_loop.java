78b493dded4374c8ca2c11f6f1bc2cdf9e4909c4
Minalzile94 <minalzile94@gmail.com>
2026-08-27 22:06:52 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,13 @@
+//syntax of while loop
+// while(condition){code}
+// here is no initialization and not a increment/decrement
+class while_loop {
+    public static void main(String[] args) {
+      //print the numbers from 1 to 10 using while loop
+      int i=1;
+      while(i<=10){
+        System.out.println(i);
+        i++;
+      }
+    }
+}
