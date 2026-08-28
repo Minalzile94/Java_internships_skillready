861f4edcf6bf093ca88896b2ecf3d6e167407317
Minalzile94 <minalzile94@gmail.com>
2026-08-28 23:03:33 +0530

    Add files via upload

---
+++ print_square.java
@@ -0,0 +1,10 @@
+//print the square from 1 to 10
+class print_square {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            System.out.println( i + "square = " +(i*i));
+            i++;
+        }
+           }
+              }
