aa0c46c3a54af0d661afa62d3f47715319609490
Minalzile94 <minalzile94@gmail.com>
2026-08-28 23:03:57 +0530

    Add files via upload

---
+++ print_cube.java
@@ -0,0 +1,12 @@
+//print the cube from 1 to 10
+class print_cube {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            System.out.println(i+" cube = "+(i*i*i));
+            i++;
+        }
+    }
+
+    
+}
