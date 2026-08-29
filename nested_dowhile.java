f9ba62c24f34f201c6eca5ecd3f48e66c3da986a
Minalzile94 <minalzile94@gmail.com>
2026-08-30 00:09:11 +0530

    Add files via upload

---
+++ nested_dowhile.java
@@ -0,0 +1,12 @@
+class nested_dowhile {
+    public static void main(String[] args) {
+        int i=1;
+        do { 
+            int j=1;
+            do{System.out.println(i+""+j);
+            j++;
+        }while (j<=3);
+        i++;
+    } while(i<=3);
+}
+}
