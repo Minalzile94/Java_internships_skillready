f5b58c98092c3be601473b56efc438ab312c1a52
Minalzile94 <minalzile94@gmail.com>
2026-08-30 00:08:00 +0530

    Add files via upload

---
+++ asciitable.java
@@ -0,0 +1,9 @@
+class asciitable {
+    public static void main(String[] args) {
+        char c ='0';
+        while(c<='Z'){
+            System.out.println(c+"="+(int)c);
+            c++;
+        }
+    }
+}
