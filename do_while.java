2035cf4d2a9c5d794165245968cb6d5c4297a213
Minalzile94 <minalzile94@gmail.com>
2026-08-30 00:08:37 +0530

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,12 @@
+class do_while {
+    public static void main(String[] args) {
+        //syntax of do while
+        //do{code}while(condition);
+        int i=1;
+        do { 
+            System.out.println(i);
+            i++;
+        } while(i<=10);
+    }
+    
+}
