4dce34f9f4d242e002f9d9e7f200d4b44d12ac15
Minalzile94 <minalzile94@gmail.com>
2026-08-30 21:58:29 +0530

    Add files via upload

---
+++ breakex.java
@@ -0,0 +1,14 @@
+//jumping statement 1st break keyword
+// continue keyword
+// break==>any programming flow or flow of the loop
+// if you want to break that then we are using the break keyword
+
+class breakex {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i==6){break;}
+            System.out.println(i);
+        }
+    }
+    
+}
