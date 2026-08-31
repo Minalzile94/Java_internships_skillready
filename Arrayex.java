0378e6899052e678b7d80bdc339867fec7e56d7e
Minalzile94 <minalzile94@gmail.com>
2026-08-31 22:35:44 +0530

    Add files via upload

---
+++ Arrayex.java
@@ -0,0 +1,20 @@
+//array==> collection of similar data type elements
+// array there are two types
+// 1st type==> one dimentional array []
+// 2nd type==> two dimentional [] []
+
+class Arrayex {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int a1[]=new int[5];
+        //int a[size]={value}
+        //a[0]=1;
+        //a[1]=2;
+        //a[2]=3;
+        //System.out.println("array value"+a[1]);
+        for(int i=0;i<5;i++){
+            System.out.println(a[i]+" ");
+    }
+    
+}
+}
