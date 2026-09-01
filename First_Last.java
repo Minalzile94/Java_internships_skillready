e1efe2a8a36226109690c4b6022ca53f4d7a9829
Minalzile94 <minalzile94@gmail.com>
2026-09-01 23:18:21 +0530

    Add files via upload

---
+++ First_Last.java
@@ -0,0 +1,15 @@
+//print the 1st element and last element in array
+// a[]{1}
+// a[]={1,2,3,4}
+//find the length of array
+class First_Last {
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5,6,7};
+        System.out.println("first element:"+a[0]);
+        System.out.println("last element:"+a[6]);
+        System.out.println("length of array:"+(a.length/2+1));
+        System.out.println("last element:"+a[a.length-1]);
+    }
+    
+}
+ 
