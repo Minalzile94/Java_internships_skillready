eb704b688d518905504c143dd7a52a23b4e4edd9
Minalzile94 <minalzile94@gmail.com>
2026-09-01 23:16:58 +0530

    Add files via upload

---
+++ Array_reverse.java
@@ -0,0 +1,13 @@
+class Array_reverse {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        for(int i=0;i<5;i++){
+            System.out.println(a[i]+" ");
+        }
+        System.out.println("reverse arrays value:");
+        for(int i=4;i>=0;i--){
+            System.out.println(a[i]+" ");
+        }
+    }
+    
+}
