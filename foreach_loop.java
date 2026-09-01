543069f2d0564d42fbbc9b07b5e8c9bcc6c09ef8
Minalzile94 <minalzile94@gmail.com>
2026-09-01 23:17:32 +0530

    Add files via upload

---
+++ foreach_loop.java
@@ -0,0 +1,11 @@
+class foreach_loop {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,50};
+        //syntax of for each loop
+        //for(variable:array_variable){}
+        for(int number:a){
+            System.out.println(number);
+        }
+    }
+    
+}
