3e8b11f76b23dfbb697c5736cf07b577fba71b6b
Minalzile94 <minalzile94@gmail.com>
2026-09-02 22:02:32 +0530

    Add files via upload

---
+++ step2.java
@@ -0,0 +1,9 @@
+class step2 {
+    public static void main(String[] args) {
+        int a[]= {1,2,3,4,5,6};
+        for (int i=0;i<a.length-2;i++){
+            System.out.println("step number"+a[i+2]);
+        }
+    }
+    
+}
