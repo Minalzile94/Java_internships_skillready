a94a4e32a9295b9d5459d148cd1b24ebd9de25fd
Minalzile94 <minalzile94@gmail.com>
2026-09-05 22:33:16 +0530

    Add files via upload

---
+++ OneD.java
@@ -0,0 +1,11 @@
+class OneD {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int addition=0;
+        for(int i=0;i<5;i++){
+            addition=addition+a[i];
+        }
+        System.out.println("addition="+addition);
+    }
+    
+}
