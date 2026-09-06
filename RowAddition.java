41ef8fe299c1aa4d6adc74babeff49276f1b9e57
Minalzile94 <minalzile94@gmail.com>
2026-09-06 21:29:39 +0530

    Add files via upload

---
+++ RowAddition.java
@@ -0,0 +1,17 @@
+class RowAddition{
+    public static void main(String[] args) {
+        int a[][]={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        for(int i=0;i<3;i++){
+            int row_Addition=0;
+            for(int j=0;j<3;j++){
+                row_Addition=row_Addition+a[i][j];
+            }
+            System.out.println("row number"+i+" addition="+row_Addition);
+        }
+    }
+
+}
