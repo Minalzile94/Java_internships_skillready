2def472fcf31fe286d1b082f2cc4f4bac775ef42
Minalzile94 <minalzile94@gmail.com>
2026-09-07 23:35:10 +0530

    Add files via upload

---
+++ StringEx.java
@@ -0,0 +1,12 @@
+//string in java is an object that represent sequence of character
+// it is a immutable can not be changed after creation
+
+class StringEx {
+    public static void main(String[] args) {
+        String s=" HELLO JAVA ";
+        s=s.concat(" Hello python ");
+        System.out.println("your string = " + s );
+
+    }
+    
+}
