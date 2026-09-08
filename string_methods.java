b36e0bf0f470473e9a316b695c36eb14de74aa77
Minalzile94 <minalzile94@gmail.com>
2026-09-08 22:27:52 +0530

    Add files via upload

---
+++ string_methods.java
@@ -0,0 +1,13 @@
+class string_methods {
+    public static void main(String[] args) {
+        String s="Java programming";
+        System.out.println("Length = "+s.length());
+        System.out.println("Character = "+s.charAt(5));
+        System.out.println("substring = "+s.substring(5));
+        System.out.println("uppercase = "+s.toUpperCase());
+        System.out.println("lowercase = "+s.toLowerCase());
+        String str = " java programming";
+        System.out.println("trim = " + str.trim());
+    }
+    
+}
