57e511108f8f7f0635fb20457c62588d616192d8
Minalzile94 <minalzile94@gmail.com>
2026-09-08 22:28:14 +0530

    Add files via upload

---
+++ Arraystring.java
@@ -0,0 +1,11 @@
+// Array with string
+class Arraystring {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        String str[]={"minal","anu", "vedu"};
+        System.out.println(str[0]);
+        System.out.println(str[1]);
+        System.out.println(str[2]);
+    }
+    
+}
