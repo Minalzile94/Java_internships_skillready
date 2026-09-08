5cb8191d67e94c425ff307e546922307454d290b
Minalzile94 <minalzile94@gmail.com>
2026-09-08 22:29:04 +0530

    Add files via upload

---
+++ Arraystring2.java
@@ -0,0 +1,10 @@
+class Arraystring2 {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        String str[]={"purvi","vishakha","samii"};
+        for (int i=0;i<3;i++){
+            System.out.println(str[i]);
+        }
+    }
+    
+}
