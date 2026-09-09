f3761b41afd2a3dbe661b09d388c651d861b2b60
Minalzile94 <minalzile94@gmail.com>
2026-09-09 21:44:37 +0530

    Add files via upload

---
+++ EmailDomain2.java
@@ -0,0 +1,12 @@
+class EmailDomain2 {
+    public static void main(String[] args) {
+        String email="aakash@gmail.com";
+        if(email.endsWith("@gmail.com")||
+            email.endsWith("@collegename.IT.edu.net")||
+            email.endsWith("@yahoo.com")){
+        System.out.println("TRUE");
+           } else {
+        System.out.println("FALSE");
+        }
+    }
+}
