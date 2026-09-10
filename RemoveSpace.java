c427e12ca776e3d0c1bb78b94539de2207a9ab23
Minalzile94 <minalzile94@gmail.com>
2026-09-10 21:32:17 +0530

    Add files via upload

---
+++ RemoveSpace.java
@@ -0,0 +1,10 @@
+ class RemoveSpace {
+    public static void main(String[] args) {
+        String name=" Minal";
+        System.out.println("my name is "+name);
+        System.out.println("using trim = "+name .trim());
+        System.out.println("my name is "+name.trim());
+        
+    }
+    
+}
