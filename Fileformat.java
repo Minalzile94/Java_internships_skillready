67396812c472fc0dca82f12a6a155340c42de4d1
Minalzile94 <minalzile94@gmail.com>
2026-09-10 21:32:46 +0530

    Add files via upload

---
+++ Fileformat.java
@@ -0,0 +1,7 @@
+class Fileformat {
+    public static void main(String[] args) {
+        String file="Resume.png";
+        System.out.println("check file format:"+file.endsWith(".pdf"));
+    }
+    
+}
