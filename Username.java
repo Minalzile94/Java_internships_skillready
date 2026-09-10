e3dd6ea666903ca13861fe896e0a3122b23faca8
Minalzile94 <minalzile94@gmail.com>
2026-09-10 21:33:06 +0530

    Add files via upload

---
+++ Username.java
@@ -0,0 +1,11 @@
+class Username {
+    public static void main(String[] args) {
+        String username="admin1";
+        if(username.equals("admin") ){
+            System.out.println("your user name is correct"); 
+            System.out.println("Login sucessyully");
+        }else{
+            System.out.println("user is incorrect");
+        }
+    }
+}
