7e863f1df17d5e2447bf2007bf25bf33976428dd
Minalzile94 <minalzile94@gmail.com>
2026-09-11 20:49:20 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,10 @@
+class Constructor {
+     Constructor()  {
+        System.out.println("Constructor call");
+
+     }
+    public static void main(String[] args) {
+        Constructor c1 = new Constructor();
+        Constructor c2 = new Constructor();
+    }
+}
