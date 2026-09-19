5132c321caeae81253e557a6c05f4dc1ced9422a
Minalzile94 <minalzile94@gmail.com>
2026-09-19 10:29:38 +0530

    Add files via upload

---
+++ encapsulationEX.java
@@ -0,0 +1,14 @@
+class student{
+    String name;
+    int age;
+}
+class encapsulationEX {
+    public static void main(String[] args) {
+        student s1=new student();
+        s1.name="minal";
+        s1.age=20;
+        System.out.println("name = "+s1.name);
+        System.out.println("age = "+s1.age);
+    }
+    
+}
