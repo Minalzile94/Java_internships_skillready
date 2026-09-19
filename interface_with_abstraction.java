be900e929138068d131803de13d2c8992d5f8033
Minalzile94 <minalzile94@gmail.com>
2026-09-19 10:30:06 +0530

    Add files via upload

---
+++ interface_with_abstraction.java
@@ -0,0 +1,14 @@
+interface vehicle{
+    void start();
+}
+class car implements vehicle{
+    public void start(){System.out.println("car starts");}
+}
+
+class interface_with_abstraction {
+    public static void main(String[] args) {
+        vehicle v1=new car();
+        v1.start();
+    }
+    
+}
